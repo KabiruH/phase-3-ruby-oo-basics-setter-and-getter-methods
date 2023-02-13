fbfd069e23ea99b852eb8fce395a22be954f1cee
King <kelkab46@gmail.com>
2023-02-13 03:15:56 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
-class Dog
 
+class Dog
+    def name=(name)
+        @name = name
+    end
+    def name
+        @name
+    end
+    def breed=(breed)
+        @breed = breed
+    end
+    def breed
+        @breed
+    end
 end
+
+blackie = Dog.new
+blackie.name = "blackie"
+blackie.breed = "pug"
